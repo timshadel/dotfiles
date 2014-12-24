3508350a02dd861ec86f7e98ef42588aaecae393
Tim Shadel <timshadel@Tims-iMac.local>
2014-12-24 13:34:50 -0700

    Customize settings for me.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,9 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "brew.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	rsync -avh --no-perms ~/init/*.otf ~/Library/Fonts/;
+	rsync -avh --no-perms ~/init/*.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/;
 	source ~/.bash_profile;
 }
 
