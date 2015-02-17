97a4ab79dda9cfe8a18ad5bc412bfab842c1a03c
Tim Shadel <tim@shadelsoftware.com>
2015-02-17 15:34:36 -0700

    Alias updates, crontab, and git exclude

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,20 +5,22 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "brew.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	rsync -avh --no-perms ~/init/*.otf ~/Library/Fonts/;
-	rsync -avh --no-perms ~/init/*.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/;
-	source ~/.bash_profile;
+  rsync --exclude ".git/" --exclude ".gitexclude" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "brew.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+  rsync -avh --no-perms ~/init/*.otf ~/Library/Fonts/;
+  rsync -avh --no-perms ~/init/*.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/;
+  source ~/.bash_profile;
+  crontab ~/.crontab;
+  cp .gitexclude .git/info/exclude;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
