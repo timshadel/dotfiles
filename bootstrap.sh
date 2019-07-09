4e263e7704ee9662bdae9413e86c4b8a2d4fc247
Tim Shadel <tim@daylogger.com>
2019-07-09 16:56:09 -0600

    Fix excludes and whitespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,27 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" \
+  rsync --exclude ".git/" \
       --exclude ".gitexclude" \
-		  --exclude ".DS_Store" \
-		  --exclude ".osx" \
-		  --exclude "bootstrap.sh" \
-		  --exclude "brew.sh" \
-		  --exclude "Brewfile" \
-		  --exclude "README.md" \
-		  --exclude "LICENSE-MIT.txt" \
-	  	-avh --no-perms . ~;
-	rsync -avh --no-perms \
+      --exclude ".DS_Store" \
+      --exclude ".osx" \
+      --exclude "bootstrap.sh" \
+      --exclude "brew.sh" \
+      --exclude "Brewfile" \
+      --exclude "dotdiff.sh" \
+      --exclude "LICENSE-MIT.txt" \
+      --exclude "NEW_MACHINE.md" \
+      --exclude "NEXT_TIME.md" \
+      --exclude "README.md" \
+      -avh --no-perms . ~;
+  rsync -avh --no-perms \
       ~/init/*.otf \
       ~/Library/Fonts/;
-	rsync -avh --no-perms \
+  rsync -avh --no-perms \
       ~/init/*.dvtcolortheme \
       ~/Library/Developer/Xcode/UserData/FontAndColorThemes/;
   rsync -avh --no-perms .path .extra .crontab ~/Dropbox/; touch ~/Dropbox/.path ~/Dropbox/.extra ~/Dropbox/.crontab
-	source ~/.bash_profile;
+  source ~/.bash_profile;
   crontab ~/.crontab;
   cp .gitexclude .git/info/exclude;
 }
